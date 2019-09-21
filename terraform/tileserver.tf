4bd204083c4b4135297b56188686b6e1f5f296f0
Unknown <git@thasler.org>
2019-09-21 08:39:02 +0200

    move tf-state to s3-backend
    lambda deployment via travis

--- terraform/tileserver.tf
+++ terraform/tileserver.tf
@@ -4,6 +4,40 @@ provider "aws" {
   region  = "${var.region}"
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-0000"  
+  versioning {
+    enabled = true
+  }  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-0000"
+    key    = "cyclemap.link/terraform.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true    
+  }
+}
+
 data "aws_vpc" "gis" {
   id = "${var.vpc_id}"
 }
